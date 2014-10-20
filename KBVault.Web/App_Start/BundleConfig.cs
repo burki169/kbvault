bdb4a1fde45e592d8dbb87ae7b0c811deb96b238
burki <burak@2bprojevi.com>
2014-10-20 09:35:07 +0300

    Tidy css/js folders

--- KBVault.Web/App_Start/BundleConfig.cs
+++ KBVault.Web/App_Start/BundleConfig.cs
@@ -8,52 +8,53 @@ namespace KBVault.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            
             bundles.Add(new ScriptBundle("~/public/js").Include(
-            "~/Assets/js/jquery-2-0-2.js",
-            "~/Assets/js/jquery-ui-1-10-4.js",
-            "~/Assets/js/bootstrap.js",
-            "~/Assets/js/jquery.cookie.js",
+            "~/Assets/js/jquery/jquery-2-0-2.js",
+            "~/Assets/js/jquery/jquery-ui-1-10-4.js",
+            "~/Assets/js/bootstrap/bootstrap.js",
+            "~/Assets/js/plugins/cookie/jquery.cookie.js",
             "~/Assets/js/frontend.js",
             "~/Assets/js/smartmenus/jquery.smartmenus.keyboard.js",
             "~/Assets/js/smartmenus/jquery.smartmenus.bootstrap.js",
             "~/Assets/js/smartmenus/jquery.smartmenus.js"
                 ));
 
+           
             
-
             bundles.Add( new StyleBundle("~/public/css").Include(
-                "~/Assets/css/bootstrap.css",
-                "~/Assets/css/font-awesome.css",
-                "~/Assets/css/ionicons.css",
+                "~/Assets/css/plugins/bootstrap/css/bootstrap.css",
+                "~/Assets/css/plugins/fontawesome/css/font-awesome.css",
+                "~/Assets/css/plugins/ionicons/css/ionicons.css",
                 "~/Assets/css/site.css",
-                "~/Assets/css/jquery.smartmenus.bootstrap.css",
-                "~/Assets/css/jquery-ui-redmond.css"
+                "~/Assets/css/plugins/smartmenus/jquery.smartmenus.bootstrap.css",
+                "~/Assets/css/plugins/jquery-ui/jquery-ui-redmond.css"
                 ));                                               
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Assets/js/jquery-2-0-2.js",
-                "~/Assets/js/jquery-ui-1-10-4.js",
-                "~/Assets/js/bootstrap.js",
-                "~/Assets/js/xeditable.js",
+            bundles.Add(new ScriptBundle("~/admin/js").Include(
+                "~/Assets/js/jquery/jquery-2-0-2.js",
+                "~/Assets/js/jquery/jquery-ui-1-10-4.js",
+                "~/Assets/js/bootstrap/bootstrap.js",
+                "~/Assets/js/plugins/x-editable/xeditable.js",
                 "~/Assets/js/AdminLTE/app.js",
                 "~/Assets/ckeditor/ckeditor.js",
-                "~/Assets/js/tag-it.js",
+                "~/Assets/js/plugins/tag-it/tag-it.js",
                 "~/Assets/datatables/js/jquery.dataTables.js",
-                "~/Assets/js/jquery.uploadfile.js",
+                "~/Assets/js/plugins/uploader/jquery.uploadfile.js",
                 "~/Assets/js/kbvault.js"
                 ));
 
             
             bundles.Add(new StyleBundle("~/admin/css").Include(
-                "~/Assets/css/bootstrap.css",
+                "~/Assets/css//plugins/bootstrap/css/bootstrap.css",
                 "~/Assets/css/jquery-ui-redmond.css",
-                "~/Assets/css/font-awesome.css",
+                "~/Assets/css/plugins/fontawesome/css/font-awesome.css",
                 "~/Assets/css/AdminLTE.css",
-                "~/Assets/css/ionicons.css",
-                "~/Assets/css/jquery.tagit.css",
-                "~/Assets/css/tagit.ui-zendesk.css",
+                "~/Assets/css/plugins/ionicons/css/ionicons.css",
+                "~/Assets/css/plugins/tagit/jquery.tagit.css",
+                "~/Assets/css/plugins/tagit/tagit.ui-zendesk.css",
                 "~/Assets/css/uploadfile.css",
-                "~/Assets/css/xeditable.css",
+                "~/Assets/css/plugins/xeditable/xeditable.css",
                 "~/Assets/datatables/css/jquery.dataTables.css",
                 "~/Assets/datatables/css/jquery.dataTables_themeroller.css",
                 "~/Assets/datatables/css/jquery.datatables.bootstrap.css"
