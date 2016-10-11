5d4fcd2379dd8a1e403abc2616087a5842883802
burki <burak@2bprojevi.com>
2016-10-11 22:50:32 +0300

    Add indexing controller

---
+++ KBVault.Web/Controllers/IndexingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KBVault.Web.Controllers
+{
+    [Authorize]
+    public class IndexingController : KbVaultAdminController
+    {
+        [Authorize(Roles = "Admin,Manager")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
