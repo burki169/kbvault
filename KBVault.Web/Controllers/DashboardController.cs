9d2134d72ab748257a5d5581c50055471b34afe6
burki <burak@2bprojevi.com>
2016-10-20 22:27:32 +0300

    Refactor Dashboard
    Refactored dashboard index controller

--- KBVault.Web/Controllers/DashboardController.cs
+++ KBVault.Web/Controllers/DashboardController.cs
@@ -5,25 +5,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KBVault.Dal.Repository;
 
 namespace KBVault.Web.Controllers
 {
     [Authorize]
     public class DashboardController : Controller
     {
+        public IArticleRepository ArticleRepository { get; set; }
         //
         // GET: /Dashboard/
 
         public ActionResult Index()
         {
-            using(var db = new KbVaultContext())
+            var model = new DashboardViewModel
             {
-                DashboardViewModel model = new DashboardViewModel();
-                model.TotalArticleCount = db.Articles.Count();
-                model.MostLikedArticle = db.Articles.OrderByDescending(a => a.Likes).FirstOrDefault();
-                model.MostViewedArticle = db.Articles.OrderByDescending(a => a.Views).FirstOrDefault();
-                return View(model);
-            }
+                TotalArticleCount = ArticleRepository.GetTotalArticleCount(),
+                MostLikedArticle = ArticleRepository.GetMostLikedArticle(),
+                MostViewedArticle = ArticleRepository.GetMostViewedArticle()
+            };
+            return View(model);
+            
         }
 
     }
